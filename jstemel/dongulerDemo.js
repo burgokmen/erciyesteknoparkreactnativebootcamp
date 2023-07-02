0b094a7c0e544cf0a84799c3a7720dd96f247b81
burgokmen <burgokmen@gmail.com>
2023-07-02 13:36:49 +0300

    Learning Process

--- jstemel/dongulerDemo.js
+++ jstemel/dongulerDemo.js
@@ -1,29 +1,26 @@
 var kullanicilar = [
-    {email:"burgokmen@gmail.com", sifre:12345},
-    {email:"bgokmen@gmail.com", sifre:12345}
-]
+  { email: "burgokmen@gmail.com", sifre: 12345 },
+  { email: "bgokmen@gmail.com", sifre: 12345 },
+];
 
 var tivitler = [
-    {email:"burgokmen@gmail.com", tivit: "bugun hava cok soguk"},
-    {email:"bgokmen@gmail.com", tivit: "bugun hava cok soguk 2"},
-    {email:"bg@gmail.com", tivit: "Kapiyi ben acicam"},
-]
+  { email: "burgokmen@gmail.com", tivit: "bugun hava cok soguk" },
+  { email: "bgokmen@gmail.com", tivit: "bugun hava cok soguk 2" },
+  { email: "bg@gmail.com", tivit: "Kapiyi ben acicam" },
+];
 
-var email = prompt("email?")
-var sifre = prompt("sifre?")
+var email = prompt("email?");
+var sifre = prompt("sifre?");
 
-function kullaniciVarmi(email, sifre){
-    console.log(email);
-    console.log(sifre);
-    for (i=0; i< kullanicilar.length; i++){
-        if(kullanicilar[i].email == email && kullanicilar[i].sifre == sifre){
-            return true;
-        }
+function kullaniciVarmi(email, sifre) {
+  console.log(email);
+  console.log(sifre);
+  for (i = 0; i < kullanicilar.length; i++) {
+    if (kullanicilar[i].email == email && kullanicilar[i].sifre == sifre) {
+      return true;
     }
-    return false;
-   
+  }
+  return false;
 }
 
-function giris(){
-    
-}
+function giris() {}
--- jstemel/index.js
+++ jstemel/index.js
@@ -1,25 +1,26 @@
 var kullanicilar = [
-    {email:"burgokmen@gmail.com", sifre:12345},
-    {email:"bgokmen@gmail.com", sifre:12345}
-]
+  { email: "burgokmen@gmail.com", sifre: 12345 },
+  { email: "bgokmen@gmail.com", sifre: 12345 },
+];
 
 var tivitler = [
-    {email:"burgokmen@gmail.com", tivit: "bugun hava cok soguk"},
-    {email:"bgokmen@gmail.com", tivit: "bugun hava cok soguk 2"},
-    {email:"bg@gmail.com", tivit: "Kapiyi ben acicam"},
-]
+  { email: "burgokmen@gmail.com", tivit: "bugun hava cok soguk" },
+  { email: "bgokmen@gmail.com", tivit: "bugun hava cok soguk 2" },
+  { email: "bg@gmail.com", tivit: "Kapiyi ben acicam" },
+];
 
-var email = prompt("email?")
-var sifre = prompt("sifre?")
+var email = prompt("email?");
+var sifre = prompt("sifre?");
 
-function giris(){
-    if((email== kullanicilar[0].email && kullanicilar[0].sifre) || 
-    (email== kullanicilar[1].email && kullanicilar[1].sifre) ){
-        console.log(tivitler)
-    }else{
-        console.log("Kullanici adi ve sifresi hatali")
-    }
-
+function giris() {
+  if (
+    (email == kullanicilar[0].email && kullanicilar[0].sifre) ||
+    (email == kullanicilar[1].email && kullanicilar[1].sifre)
+  ) {
+    console.log(tivitler);
+  } else {
+    console.log("Kullanici adi ve sifresi hatali");
+  }
 }
 
-giris(email, sifre)
+giris(email, sifre);
